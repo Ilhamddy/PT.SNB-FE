9a26c0d875ee7e474b0eadb3efd33ce61828eef4
aleslesmana <aseplesmana664@gmail.com>
2023-05-08 18:45:58 +0700

    -

--- backendjs/app/config/db.config.js
+++ backendjs/app/config/db.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // USER: "postgres",
     // PASSWORD: "@b3rd1k4r1@",
     // DB: "standart_v1",
-    HOST: "localhost",
+    HOST: "103.149.177.11",
     USER: "postgres",
     PASSWORD: "@b3rd1k4r1@",
     DB: "standart_v1",
