48ef0be844aa41dbec0092e8d673a3a24cb6b9a2
aleslesmana <aseplesmana664@gmail.com>
2023-05-08 20:12:09 +0700

    -

--- backendjs/app/queries/master/agama/agama.queries.js
+++ backendjs/app/queries/master/agama/agama.queries.js
@@ -1,5 +1,5 @@
 const getAll =
-    "SELECT id, statusenabled, kodeexternal, namaexternal, reportdisplay, agama, kdagama FROM agama_m where statusenabled = true";
+    "SELECT id, statusenabled, kodeexternal, namaexternal, reportdisplay, agama, kdagama FROM m_agama where statusenabled = true";
 
 module.exports = {
     getAll
