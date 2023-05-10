8569b8ae8567b6c70e03e5163115e0dcb0a70e5f
aleslesmana <aseplesmana664@gmail.com>
2023-05-10 14:10:07 +0700

    -

--- backendjs/app/config/auth.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    secret: "snb-secret-key"
-  };
--- backendjs/app/queries/master/agama/agama.queries.js
+++
@@ -1,6 +0,0 @@
-const getAll =
-    "SELECT id, statusenabled, kodeexternal, namaexternal, reportdisplay, agama, kdagama FROM m_agama where statusenabled = true";
-
-module.exports = {
-    getAll
-};
