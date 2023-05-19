bef147e0fc741640af0d601a7c220c9e0c492c21
aleslesmana <aseplesmana664@gmail.com>
2023-05-19 16:55:36 +0700

    -

--- frontendjs/src/config.js
+++ frontendjs/src/config.js
@@ -8,6 +8,6 @@ module.exports = {
     APP_ID: "",
   },
   api: {
-    API_URL: process.env.REACT_APP_API_URL,//"http://localhost:8085/api",
+    API_URL: "http://103.149.177.11:8085/api",
   }
 };
