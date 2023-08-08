452242d719dad07a18a399ea16d5c1c75e8e97b1
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2023-08-08 17:20:24 +0700

    typing default

--- backendjs/app/utils/dbutils.js
+++ backendjs/app/utils/dbutils.js
@@ -1,17 +1,16 @@
 
 /**
  * 
- * @param {any} db 
+ * @param {typeof import("../models/index").default} db 
  * @param {any} res 
  * @param {string} [transactionName] 
  * transaction name is optional
- * @returns {Promise<[import("sequelize").Transaction, any]>}
  */
 export const createTransaction = async (db, res, transactionName) => {
     let transaction = null;
     let error = null
     try {
-        transaction = await db.sequelize.transaction();
+        transaction = await db.sequelize.transaction()
     } catch (e) {
         res.status(500).send({
             status: JSON.stringify(e),
