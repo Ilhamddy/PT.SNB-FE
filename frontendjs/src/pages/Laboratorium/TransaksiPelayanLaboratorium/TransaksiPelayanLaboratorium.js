054086ed5021001b559a7ddbf36bf2567cca5550
Asep Lesmana <aseplesmana664@gmail.com>
2023-08-09 13:41:06 +0700

    comite asep

--- frontendjs/src/pages/Laboratorium/TransaksiPelayanLaboratorium/TransaksiPelayanLaboratorium.js
+++ frontendjs/src/pages/Laboratorium/TransaksiPelayanLaboratorium/TransaksiPelayanLaboratorium.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 import {
     Card, CardBody, CardHeader, Col, Container, Row, Nav, NavItem,
     NavLink, TabContent, TabPane, Button, Label, Input, Table
@@ -15,15 +15,31 @@ import {
     listPelayananLaboratoriumGet, laboratoriumResetForm
 } from '../../../store/actions';
 import classnames from "classnames";
+import { useFormik } from 'formik';
 const TransaksiPelayanLaboratorium = () => {
     const { norecdp, norecap } = useParams();
     const dispatch = useDispatch();
     document.title = "Transaksi Pelayan Laboratorium";
     const { dataPelayanan, loadingPelayanan, successPelayanan } = useSelector((state) => ({
-        dataPelayanan: state.Laboratorium.listPelayananLaboratoriumGet.data,
+        dataPelayanan: state.Laboratorium.listPelayananLaboratoriumGet.data || [],
         loadingPelayanan: state.Laboratorium.listPelayananLaboratoriumGet.loading,
         successPelayanan: state.Laboratorium.listPelayananLaboratoriumGet.success,
     }));
+    const validation = useFormik({
+        enableReinitialize: true,
+        initialValues: {
+            pelayananproses: []
+        },
+        onSubmit: (value) => {
+
+        }
+    });
+    useEffect(() => {
+        const setFF = validation.setFieldValue
+        if((dataPelayanan || []).length !== 0){
+            setFF("pelayananproses", dataPelayanan)
+        }
+    }, [dataPelayanan, validation.setFieldValue])
     useEffect(() => {
         return () => {
             dispatch(laboratoriumResetForm());
@@ -145,6 +161,28 @@ const TransaksiPelayanLaboratorium = () => {
         },
 
     ];
+
+    const handleInputChangeHasil = (data,id, nama, value, nodata) => {
+        let newData = [...data]
+
+        for (let i = 0; i < newData.length; i++) {
+            if(newData[i].idnilainormallab===id){
+                newData[i].nilaihasil=value
+            }
+        }
+
+        let newDataPelayanan = [...validation.values.pelayananproses]
+
+        const indexChange = newDataPelayanan.findIndex((newDataPel) => newDataPel === nodata)
+        if(indexChange >= 0){
+            newDataPelayanan[indexChange] = newData
+
+        }
+
+        validation.setFieldValue("pelayananproses", newDataPelayanan)
+    };
+
+    
     return (
         <React.Fragment>
             <UiContent />
@@ -158,36 +196,40 @@ const TransaksiPelayanLaboratorium = () => {
                         <Col ccl={12}>
                             <Card>
                                 <CardBody>
-                                <div className="card-header align-items-center d-flex">
-                                    <Nav tabs className="nav justify-content-end nav-tabs-custom rounded card-header-tabs border-bottom-0">
-                                        {taskWidgets.map((item, key) => (
-                                            <NavItem key={key}>
-                                                <NavLink style={{ cursor: "pointer" }} className={classnames({ active: pillsTab === `${item.id}`, })} onClick={() => { pillsToggle(`${item.id}`); }}>
-                                                    <span className="fw-semibold">{item.label}</span>
-                                                </NavLink>
-                                            </NavItem>
-                                        ))}
-                                    </Nav>
+                                    <div className="card-header align-items-center d-flex">
+                                        <Nav tabs className="nav justify-content-end nav-tabs-custom rounded card-header-tabs border-bottom-0">
+                                            {taskWidgets.map((item, key) => (
+                                                <NavItem key={key}>
+                                                    <NavLink style={{ cursor: "pointer" }} className={classnames({ active: pillsTab === `${item.id}`, })} onClick={() => { pillsToggle(`${item.id}`); }}>
+                                                        <span className="fw-semibold">{item.label}</span>
+                                                    </NavLink>
+                                                </NavItem>
+                                            ))}
+                                        </Nav>
                                     </div>
                                     <TabContent activeTab={pillsTab} className="text-muted">
                                         <TabPane tabId="1" id="home-1">
                                             <Card>
                                                 <CardBody>
-                                            <div id="table-gridjs">
-                                                
-                                                <DataTable
-                                                    fixedHeader
-                                                    fixedHeaderScrollHeight="400px"
-                                                    columns={columns}
-                                                    pagination
-                                                    data={dataPelayanan}
-                                                    progressPending={loadingPelayanan}
-                                                    customStyles={tableCustomStyles}
-                                                    expandableRows
-                                                    expandableRowsComponent={ExpandableNilaiNormal}
-                                                />
-                                            </div>
-                                            </CardBody>
+                                                    <div id="table-gridjs">
+                                                        <DataTable
+                                                            fixedHeader
+                                                            fixedHeaderScrollHeight="400px"
+                                                            columns={columns}
+                                                            pagination
+                                                            data={validation.values.pelayananproses}
+                                                            progressPending={loadingPelayanan}
+                                                            customStyles={tableCustomStyles}
+                                                            expandableRows
+                                                            expandableRowsComponent={({...rest}) => 
+                                                                <ExpandableNilaiNormal
+                                                                    handleInputChangeHasil={handleInputChangeHasil}
+                                                                    {...rest}
+                                                                />
+                                                            }
+                                                        />
+                                                    </div>
+                                                </CardBody>
                                             </Card>
                                         </TabPane>
                                         <TabPane tabId="2" id="home-1">
@@ -205,33 +247,84 @@ const TransaksiPelayanLaboratorium = () => {
     )
 }
 
-const ExpandableNilaiNormal = ({ data }) => {
 
-    if(data.listnilainormal.length === 0 ){
+const handleClickSave = (e) => {
+   console.log(e)
+};
+
+const ExpandableNilaiNormal = ({ data, handleInputChangeHasil }) => {
+    const [tempVal, setTempValue] = useState({
+        index: -1,
+        value: "",
+    })
+    if (data.listnilainormal.length === 0) {
         return <></>
     }
     return (
         <table className="table">
             <thead className="thead-light">
                 <tr>
-                    <th scope="col">Detail Pemeriksaan</th>
-                    <th scope="col">Hasil Pemeriksaan</th>
-                    <th scope="col">Nilai Normal</th>
-                    <th scope="col">Satuan Hasil</th>
-                    <th scope="col">Metode</th>
-                    <th scope="col">Keterangan</th>
+                    <th style={{ width: '50px' }} scope="col">Detail Pemeriksaan</th>
+                    <th style={{ width: '100px' }} scope="col">Hasil Pemeriksaan</th>
+                    <th style={{ width: '50px' }} scope="col">Nilai Normal</th>
+                    <th style={{ width: '50px' }} scope="col">Satuan Hasil</th>
+                    <th style={{ width: '50px' }} scope="col">Metode</th>
+                    <th style={{ width: '100px' }} scope="col">Keterangan</th>
                 </tr>
             </thead>
             <tbody>
 
-            {data.listnilainormal.map((item, key) =>
-                <tr key={key}>
-                    <th scope="row">{key + 1}</th>
-                    <td>{item.reportdisplay}</td>
-                    {/* <td>{item.nominal?.toLocaleString("id-ID") || ""}</td>
+                {data.listnilainormal.map((item, key) =>
+                    <tr key={key}>
+                        {/* <th scope="row">{key + 1}</th> */}
+                        <td>{item.reportdisplay}</td>
+                        <td>
+                            <input
+                            type="text"
+                            name="nilaihasil"
+                            value={key === tempVal.index ? tempVal.value : item.nilaihasil}
+                            placeholder="Nilai Hasil"
+                            className="form-control"
+                            onFocus={(e) => setTempValue({
+                                index: key,
+                                value: e.target.value
+                            })}
+                            onBlur={(e) => handleInputChangeHasil(
+                                data.listnilainormal,
+                                item.idnilainormallab, 
+                                'nilaihasil',
+                                e.target.value,
+                                data.no,
+                            )}
+                            // disabled={row.statusDisable}
+                            onChange={(e) => {
+                                key === tempVal.index &&
+                                    setTempValue({
+                                        index: tempVal.index,
+                                        value: e.target.value
+                                    })
+                            }}
+                        />
+                        </td>
+                        <td>{item.nilaitext}</td>
+                        <td>{item.satuan}</td>
+                        <td>{item.metodepemeriksaan}</td>
+                        <td>{item.keterangan}</td>
+                        {/* <td>{item.nominal?.toLocaleString("id-ID") || ""}</td>
                     <td>{item.nobukti}</td> */}
+                    </tr>
+                )}
+                <tr>
+                    <td style={{ textAlign: 'center' }} colSpan={6}>
+                        <Button type="button" style={{ backgroundColor: '#192a56', textAlign: 'right' }} placement="top"
+                        onClick={() => handleClickSave(data.listnilainormal)}>
+                            Simpan
+                        </Button>
+                        <Button type="button" style={{ backgroundColor: 'red', textAlign: 'right' }} placement="top">
+                            Batal
+                        </Button>
+                    </td>
                 </tr>
-            )}
             </tbody>
         </table>
 
