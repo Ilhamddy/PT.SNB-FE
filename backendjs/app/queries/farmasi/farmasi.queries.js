f6a6d20e9bc35d0fc2159149f352f0f28205cfe8
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2023-08-29 16:54:29 +0700

    fix product batch

--- backendjs/app/queries/farmasi/farmasi.queries.js
+++ backendjs/app/queries/farmasi/farmasi.queries.js
@@ -21,7 +21,7 @@ FROM t_stokunit tsu
     LEFT JOIN m_satuan ms ON ms.id = mp.objectsatuanstandarfk
     LEFT JOIN m_sediaan msd ON msd.id = mp.objectsediaanfk
 WHERE tsu.objectprodukfk = $1
-    tsu.objectunitfk = $2
+    AND tsu.objectunitfk = $2
     AND tsu.qty > 0
     AND tsu.statusenabled = true
 GROUP BY 
