4afa0dd067d86fcca03d860ec0091f3db835bcc0
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2023-10-10 10:32:20 +0700

    back akun

--- daftarmandiri/src/pages/AkunPage/AkunPage.jsx
+++ daftarmandiri/src/pages/AkunPage/AkunPage.jsx
@@ -8,6 +8,7 @@ import ExpandRight from './expand-right.svg'
 import { useNavigate } from 'react-router-dom'
 import UserImg from './user.svg'
 import DokterTemp from './doktercuplik.png'
+import BackKomponen from '../../Components/BackKomponen/BackKomponen'
 
 const AkunPage = () => {
   const { userAkun } = useSelector((state) => ({
@@ -27,32 +28,35 @@ const AkunPage = () => {
       classNameKonten="konten-akun"
     >
       <div className="akun-kontainer">
-        <div className="opsi-akun">
-          <div className="data-akun">
-            <img className="gbr-akun" alt="" src={DokterTemp} />
-            <h2 className="nama-pengguna">{userAkun?.namalengkap || '-'}</h2>
-            <p className="data">{userAkun?.nocm || userAkun?.nocmtemp}</p>
-            <p className="data">{userAkun?.nohp}</p>
-          </div>
+        <div>
+          <BackKomponen text={'Akun'} refKontainer={refKontainer} />
+          <div className="opsi-akun">
+            <div className="data-akun">
+              <img className="gbr-akun" alt="" src={DokterTemp} />
+              <h2 className="nama-pengguna">{userAkun?.namalengkap || '-'}</h2>
+              <p className="data">{userAkun?.nocm || userAkun?.nocmtemp}</p>
+              <p className="data">{userAkun?.nohp}</p>
+            </div>
 
-          <ButtonOpsi
-            onClick={() => {
-              refKontainer.current.handleToNextPage(() => {
-                navigate('/akun/edit')
-              })
-            }}
-          >
-            Edit Data Pasien
-          </ButtonOpsi>
-          <ButtonOpsi
-            onClick={() => {
-              refKontainer.current.handleToNextPage(() => {
-                navigate('/akun/penjamin')
-              })
-            }}
-          >
-            Penjamin Pasien
-          </ButtonOpsi>
+            <ButtonOpsi
+              onClick={() => {
+                refKontainer.current.handleToNextPage(() => {
+                  navigate('/akun/edit')
+                })
+              }}
+            >
+              Edit Data Pasien
+            </ButtonOpsi>
+            <ButtonOpsi
+              onClick={() => {
+                refKontainer.current.handleToNextPage(() => {
+                  navigate('/akun/penjamin')
+                })
+              }}
+            >
+              Penjamin Pasien
+            </ButtonOpsi>
+          </div>
         </div>
         <ButtonDM onClick={() => dispatch(logoutUser())}>Keluar</ButtonDM>
       </div>
