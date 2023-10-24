9d9008d4257e13fe8d8556a07f0183ba35d0ec3c
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2023-10-24 10:23:51 +0700

    detail pendaftaran

--- daftarmandiri/src/pages/DetailPendaftaran/DetailPendaftaran.jsx
+++ daftarmandiri/src/pages/DetailPendaftaran/DetailPendaftaran.jsx
@@ -1,1 +1,16 @@
-const DetailPendaftaran = () => {}
+import { useRef } from 'react'
+import BackKomponen from '../../Components/BackKomponen/BackKomponen'
+import KontainerPage from '../../Components/KontainerPage/KontainerPage'
+
+const DetailPendaftaran = () => {
+  const refKontainer = useRef(null)
+  return (
+    <KontainerPage top={'0'} ref={refKontainer} className="detail-pendaftaran">
+      <div className="page-detail-pendaftaran">
+        <BackKomponen refKontainer={refKontainer} text="Pilih Jadwal" />
+        <img />
+        <div></div>
+      </div>
+    </KontainerPage>
+  )
+}
