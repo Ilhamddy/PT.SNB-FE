3d0f018884569f109bf077f00c24ddf5849263f1
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2023-11-08 16:24:59 +0700

    commit

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,22 @@
 cd %~dp0
-git checkout develop-deploy
-git merge develop
-git pull
-git push
-git checkout 
+git checkout develop-deploy || {
+    echo Kesalahan checkout
+    exit /b 1
+}
+git merge develop || {
+    echo Kesalahan merge
+    exit /b 1
+}
+git pull || {
+    echo Kesalahan pull
+    exit /b 1
+}
+git commit -am "Merge branch remote to local"
+git push || {
+    echo Kesalahan push
+    exit /b 1
+}
+git checkout develop || {
+    echo Kesalahan checkout
+    exit /b 1
+}
