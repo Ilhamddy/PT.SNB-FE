76453b11599f89f60a2a07592733eb82b06f4d69
Asep Lesmana <aseplesmana664@gmail.com>
2023-11-21 14:39:49 +0700

    -

--- frontendjs/src/Components/Skala/Skala.jsx
+++ frontendjs/src/Components/Skala/Skala.jsx
@@ -1,16 +1,5 @@
-import skala0 from './skala0.svg'
-import skala1 from './skala1.svg'
-import skala2 from './skala2.svg'
-import skala3 from './skala3.svg'
-import skala4 from './skala4.svg'
-import skala5 from './skala5.svg'
-import skala6 from './skala6.svg'
-import skala7 from './skala7.svg'
-import skala8 from './skala8.svg'
-import skala9 from './skala9.svg'
-import skala10 from './skala10.svg'
 import { useState } from 'react'
-import './SkalaNyeri.scss'
+import './Skala.scss'
 
 /**
  * @typedef {object} Props
@@ -21,61 +10,50 @@ import './SkalaNyeri.scss'
 /**
  * @type {import('react').FC<Props>}
  */
-const SkalaNyeri = ({ quantity, onQuantityChange }) => {
+const Skala = ({ quantity, onQuantityChange }) => {
   const skalas = [
     {
       skala: 0,
-      img: skala0,
       color: '#308A45',
     },
     {
       skala: 1,
-      img: skala1,
       color: '#4F9A3F',
     },
     {
       skala: 2,
-      img: skala2,
       color: '#8CBB33',
     },
     {
       skala: 3,
-      img: skala3,
       color: '#CBDE27',
     },
     {
       skala: 4,
-      img: skala4,
       color: '#F3EA1E',
     },
     {
       skala: 5,
-      img: skala5,
       color: '#F3CF16',
     },
     {
       skala: 6,
-      img: skala6,
       color: '#F3B410',
     },
     {
       skala: 7,
-      img: skala7,
       color: '#F39208',
     },
     {
       skala: 8,
-      img: skala8,
       color: '#EE6100',
     },
     {
       skala: 9,
-      img: skala9,
       color: '#E33B00',
     },
     {
       skala: 10,
-      img: skala10,
       color: '#D40B0B',
     },
   ]
@@ -85,11 +63,10 @@ const SkalaNyeri = ({ quantity, onQuantityChange }) => {
   return (
     <div className="kontainer-skala-nyeri">
       {skalas.map((skala, key) => (
-        <IsiSkalaNyeri
+        <IsiSkala
           key={key}
           skala={skala.skala}
-          img={skala.img}
-          color={skala.color}
+          color='#36AE7C'//{skala.color}
           filled={skala.skala <= quantity}
           onClick={() => handleValueChange(skala.skala)}
         />
@@ -98,23 +75,18 @@ const SkalaNyeri = ({ quantity, onQuantityChange }) => {
   )
 }
 
-const IsiSkalaNyeri = ({ skala, img, color, filled, onClick }) => {
+const IsiSkala = ({ skala, color, filled, onClick }) => {
   const backgroundStyle = !filled
     ? {}
     : {
       backgroundColor: color,
     }
   return (
-    <div className="kontainer-isi-skala-nyeri" style={backgroundStyle}>
-      <img
-        onClick={onClick}
-        className="gbr-skala-nyeri"
-        src={img}
-        alt={skala}
-      />
-      <p className="teks-skala">{skala}</p>
+    <div className="kontainer-isi-skala-nyeri" style={backgroundStyle} onClick={onClick}>
+
+      <p className="teks-skala" style={{ color: 'black' }}>{skala}</p>
     </div>
   )
 }
 
-export default SkalaNyeri
+export default Skala
