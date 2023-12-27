6f740e3c3e3e86bbc937f19f836a41af020dbb7a
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2023-12-27 08:49:43 +0700

    komentar ss

--- backendjs/app/controllers/transaksi/satuSehat/satuSehat.helper.js
+++ backendjs/app/controllers/transaksi/satuSehat/satuSehat.helper.js
@@ -1,60 +1,60 @@
 import { createLogger } from "../../../utils/logger"
 
-const optionalObat = {
-    "manufacturer": {
-        "reference": "Organization/900001"
-    },
-    "form": {
-        "coding": [
-            {
-                "system": "http://terminology.kemkes.go.id/CodeSystem/medication-form",
-                "code": "BS023",
-                "display": "Kaplet Salut Selaput"
-            }
-        ]
-    },
-}
+// const optionalObat = {
+//     "manufacturer": {
+//         "reference": "Organization/900001"
+//     },
+//     "form": {
+//         "coding": [
+//             {
+//                 "system": "http://terminology.kemkes.go.id/CodeSystem/medication-form",
+//                 "code": "BS023",
+//                 "display": "Kaplet Salut Selaput"
+//             }
+//         ]
+//     },
+// }
 
-const optionalOrderObat = {
-    "courseOfTherapyType": {
-        "coding": [
-            {
-                "system": "http://terminology.hl7.org/CodeSystem/medicationrequest-course-of-therapy",
-                "code": "continuous",
-                "display": "Continuing long term therapy"
-            }
-        ]
-    },
-    "dispenseRequest": {
-        "dispenseInterval": {
-            "value": 1,
-            "unit": "days",
-            "system": "http://unitsofmeasure.org",
-            "code": "d"
-        },
-        "validityPeriod": {
-            "start": "2022-01-01",
-            "end": "2022-01-30"
-        },
-        "numberOfRepeatsAllowed": 0,
-        "quantity": {
-            "value": 120,
-            "unit": "TAB",
-            "system": "http://terminology.hl7.org/CodeSystem/v3-orderableDrugForm",
-            "code": "TAB"
-        },
-        "expectedSupplyDuration": {
-            "value": 30,
-            "unit": "days",
-            "system": "http://unitsofmeasure.org",
-            "code": "d"
-        },
-        "performer": {
-            "reference": "Organization/10000004"
-        }
-    },
+// const optionalOrderObat = {
+//     "courseOfTherapyType": {
+//         "coding": [
+//             {
+//                 "system": "http://terminology.hl7.org/CodeSystem/medicationrequest-course-of-therapy",
+//                 "code": "continuous",
+//                 "display": "Continuing long term therapy"
+//             }
+//         ]
+//     },
+//     "dispenseRequest": {
+//         "dispenseInterval": {
+//             "value": 1,
+//             "unit": "days",
+//             "system": "http://unitsofmeasure.org",
+//             "code": "d"
+//         },
+//         "validityPeriod": {
+//             "start": "2022-01-01",
+//             "end": "2022-01-30"
+//         },
+//         "numberOfRepeatsAllowed": 0,
+//         "quantity": {
+//             "value": 120,
+//             "unit": "TAB",
+//             "system": "http://terminology.hl7.org/CodeSystem/v3-orderableDrugForm",
+//             "code": "TAB"
+//         },
+//         "expectedSupplyDuration": {
+//             "value": 30,
+//             "unit": "days",
+//             "system": "http://unitsofmeasure.org",
+//             "code": "d"
+//         },
+//         "performer": {
+//             "reference": "Organization/10000004"
+//         }
+//     },
 
-}
+// }
 
 export const wrapperSatuSehat = (callback) => async (...rest) => {
     const logger = createLogger("SATU SEHAT")
@@ -64,6 +64,5 @@ export const wrapperSatuSehat = (callback) => async (...rest) => {
     } catch (error) {
         logger.error(error)
     }
-
 }
 
