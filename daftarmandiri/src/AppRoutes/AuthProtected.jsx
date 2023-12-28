87b5d4b703c6b36654d189ea8fd189723f6da992
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2023-12-28 14:00:57 +0700

    auth protected jsx

---
+++ daftarmandiri/src/AppRoutes/AuthProtected.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import { Navigate, Route } from 'react-router-dom'
+import { setAuthorization } from '../helpers/api_helper'
+import { useDispatch, useSelector } from 'react-redux'
+
+const AuthProtected = (props) => {
+  const { userid } = useSelector((state) => {
+    const user = localStorage.getItem('authUserMandiri')
+      ? JSON.parse(localStorage.getItem('authUserMandiri'))
+      : null
+
+    return {
+      userid: state.UserPasien.loginUser?.data?.id || user?.id || null,
+    }
+  })
+
+  if (!userid) {
+    return <Navigate to={{ pathname: '/login/pasien-lama' }} replace />
+  }
+
+  return <>{props.children}</>
+}
+
+const AccessRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return (
+          <>
+            {' '}
+            <Component {...props} />{' '}
+          </>
+        )
+      }}
+    />
+  )
+}
+
+export { AuthProtected, AccessRoute }
