721a169bc7e7ed8a419b5f9c83780161512cb887
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2024-01-18 15:30:44 +0700

    std error

--- backendjs/app/controllers/transaksi/system/system.controller.js
+++ backendjs/app/controllers/transaksi/system/system.controller.js
@@ -24,9 +24,8 @@ const pullGit = async (req, res) => {
             let lastUpdatedFile = __dirname + "/lastupdated.log"
             logger.info("masuk write file")
             logger.info("coba stderr")
-            logger.info(typeof !(stderr === " "))
-            logger.info( !(stderr === " "))
-            
+            logger.info(encodeURIComponent(stderr))
+
             if(!stderr){
                 writeFileSync(lastUpdatedFile, new Date().toISOString())
             }
