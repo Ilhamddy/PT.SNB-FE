f6a149e06b6b253ab48966d0cbd2ff901b32aba4
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2024-02-05 09:11:44 +0700

    logout

--- backendjs/app/middleware/encryptMandiri.js
+++ backendjs/app/middleware/encryptMandiri.js
@@ -70,19 +70,6 @@ export const encryptMandiri = async (req, res, next) => {
                 });
             }
         }
-        // let oldJson = res.json;    
-        // res.json = function(data) {
-        //     try{
-        //         const newData = encrypt(data, clientSecret)
-        //         res.json = oldJson 
-        //         return res.json(newData) 
-        //     }catch(e){
-        //         logger.error(e)
-        //         res.status(401).send({
-        //             msg: "Unauthorized!"
-        //         });
-        //     }
-        // }
     
         next();
     }catch(e){
--- frontendjs/src/helpers/api_helper.js
+++ frontendjs/src/helpers/api_helper.js
@@ -25,14 +25,14 @@ axios.interceptors.response.use(
         message = "Internal Server Error";
         break;
       case 401:
-        window.location = '/auth-logout-basic'
+        window.location = '/logout'
         message = "Invalid credentials";
         break;
       case 404:
         message = "Sorry! the data you are looking for could not be found";
         break;
       case 403:
-        window.location = '/auth-logout-basic'
+        window.location = '/logout'
         message = "Sorry! the data you are looking for could not be found";
         break;
       default:
