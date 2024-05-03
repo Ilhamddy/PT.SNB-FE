0558ca5cbdc8862e4c4fa3b22aca877ba0680ab4
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2024-05-03 09:08:46 +0700

    odonto gigi

--- backendjs/app/queries/penunjang/gigi/gigi.queries.js
+++ backendjs/app/queries/penunjang/gigi/gigi.queries.js
@@ -3,7 +3,8 @@
 const qGetAllGigi = `
 SELECT
     mg.reportdisplay AS reportdisplay,
-    mg.id AS idgigi
+    mg.id AS idgigi,
+    mg.isseri AS isseri
 FROM m_gigi mg
 WHERE mg.statusenabled = TRUE
 `
