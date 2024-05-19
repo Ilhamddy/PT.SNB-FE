441175d97e543374703554230f0a5500133c7d14
ilhamddy <ilhammaulana9906@gmail.com>
2024-05-19 23:05:26 +0800

    fix: fix feature image

--- src/app/news/[newsDetail]/component/NewsDetail.tsx
+++ src/app/news/[newsDetail]/component/NewsDetail.tsx
@@ -19,51 +19,60 @@ interface INews {
 }
 
 const NewsDetail = () => {
-  const getNewsById: any = useGetNewsById();
-  console.log(getNewsById);
 
-  const getNews = useNews();
+    const getNewsById: any = useGetNewsById();
+    console.log(getNewsById);
 
-  return (
-    <section className="bg-third">
-      <div className="mx-3 h-full bg-cover py-10 sm:mx-10 md:mx-10">
-        <div className="grid grid-cols-7 sm:mx-10 md:mx-10">
-          <div className="col-span-5 my-10 grid grid-cols-1 gap-5 text-center sm:grid-cols-1 md:mx-10 md:grid-cols-1">
-            <div className="flex justify-center"></div>
+    const getNews = useNews();
 
-            <div className="grid grid-cols-1 ">
-              <h1 className="my-5 text-justify text-4xl font-bold text-dope">
-                {getNewsById.title}{" "}
-                {/* Access title property from getNewsById */}
-              </h1>
-              {HTMLReactParser(getNewsById.description || "")}
-            </div>
-            <p className="flex justify-between text-end">
-              <div className="text-[10px]">Source: {getNewsById.source} </div>
 
-              {getNewsById.createdAt
-                ? format(new Date(getNewsById.createdAt), "MMMM dd, yyyy")
-                : "Date not available"}
-            </p>
-          </div>
-          <div className="col-span-2 my-10">
-            <div className="grid grid-cols-1 ">
-              {getNews.length === 0 ? (
-                <div>Loading...</div> // Display loading indicator or message
-              ) : (
-                getNews.map((news: INews, index: number) => (
-                  <Card className="my-2" key={index}>
-                    {/* <Link href={`/news/${news.id}`} key={index}> */}
+    return (
+        <section className="bg-third">
+            <div className="mx-3 h-full bg-cover py-10 sm:mx-10 md:mx-10">
+                <div className="sm:mx-10 md:mx-10 grid grid-cols-7">
+                    <div className="my-10 grid grid-cols-1 gap-5 text-justify sm:grid-cols-1 md:mx-10 md:grid-cols-1 col-span-5">
+                        <div className="flex justify-center">
+                            <Image
+                                className="my-5 h-64 w-[1000px]  rounded-xl sm:h-[500px] md:my-0 md:h-[550px]"
+                                height={1500}
+                                width={1500}
+                                quality={100}
+                                alt="Kemenkes SNB"
+                                src={`${baseImage}/news/${getNewsById.image}`}
+                            />
+                        </div>
 
-                    <div className="flex  justify-between gap-5 p-5">
-                      {/*<Image
+                        <div className="grid grid-cols-1 ">
+                            <h1 className="my-5 text-justify text-dope font-bold text-4xl">
+                                {getNewsById.title} {/* Access title property from getNewsById */}
+                            </h1>
+    {HTMLReactParser(getNewsById.description || "")}
+
+                        </div>
+                        <p className="text-end flex justify-between">
+                        <div  className="text-[10px]">Source: {getNewsById.source} </div>
+
+                            {getNewsById.createdAt ? format(new Date(getNewsById.createdAt), 'MMMM dd, yyyy') : 'Date not available'}
+                        </p>
+                    </div>
+                    <div className="col-span-2 my-10">
+                        <div className="grid grid-cols-1 ">
+                            {getNews.length === 0 ? (
+                                <div>Loading...</div> // Display loading indicator or message
+                            ) : (
+                                getNews.map((news: INews, index: number) => (
+                                    <Card className="my-2" key={index}>
+                                        {/* <Link href={`/news/${news.id}`} key={index}> */}
+
+                                        <div className="flex  justify-between p-5 gap-5">
+                                            <Image
                                                 src={`${baseImage}/news/${news.image}`}
                                                 alt=""
                                                 width={100}
                                                 height={200}
                                                 quality={100}
                                                 className="rounded-xl text-center text-3xl  md:text-start md:text-5xl"
-                                            />*/}
+                                            />
                       <div className="grid">
                         <div>
                           <h2 className="my-1 text-xl">{news.title}.</h2>
