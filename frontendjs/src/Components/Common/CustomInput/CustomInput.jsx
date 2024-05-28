0c4a05578872b0e3072ae1a54ead0583879f0638
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2024-05-28 14:46:50 +0700

    penjagaan kirim

--- frontendjs/src/Components/Common/CustomInput/CustomInput.jsx
+++ frontendjs/src/Components/Common/CustomInput/CustomInput.jsx
@@ -2,9 +2,6 @@ import { forwardRef, FC } from 'react'
 import { FormFeedback, Input } from 'reactstrap'
 /**
  * @typedef {object} Props
- * @prop {boolean} [isFormFeedback]
- * @prop {boolean} [isTouched]
- * @prop {string} errorMessage
  */
 /**
  * @type {FC<import('reactstrap').InputProps & Props>}
@@ -14,19 +11,7 @@ const CustomInput = forwardRef(
   (
     { isFormFeedback = false, isTouched = false, errorMessage = null, ...rest },
     ref
-  ) =>
-    isFormFeedback ? (
-      <Input ref={ref} {...rest} />
-    ) : (
-      <>
-        <Input ref={ref} invalid={isTouched && !!errorMessage} {...rest} />
-        {isTouched && !!errorMessage && (
-          <FormFeedback type="invalid">
-            <div>{errorMessage}</div>
-          </FormFeedback>
-        )}
-      </>
-    )
+  ) => <Input ref={ref} {...rest} />
 )
 
 CustomInput.displayName = 'CustomInput'
