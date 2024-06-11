0bf05255f483833ac6b6b72cccdba2184187e466
Disky Aria Jetmiko <disky.jetmiko@gmail.com>
2024-06-11 12:22:07 +0700

    upload berita

--- frontendjs/src/store/berita/saga.js
+++ frontendjs/src/store/berita/saga.js
@@ -63,7 +63,7 @@ function* onUploadImageSuccess({payload: {dataSend, dataResp}}) {
 
 function* onUploadBerita({payload: {data}}) {
     try {
-        const response = yield call(serviceADM.getBerita, {});
+        const response = yield call(serviceADM.uploadBerita, data);
         toast.success('Upload Berita Berhasil')
         yield put(uploadBeritaSuccess(response.data));
         toast.success('Upload Berita Berhasil')
